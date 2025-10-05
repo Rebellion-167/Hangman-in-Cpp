cf02eeddf030d49a7371c891023fa063be971a70
Barnik Chakraborty <barnikc79@gmail.com>
2025-10-05 20:26:14 +0530

    Updated the driver code

--- game_code.cpp
+++ game_code.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "print_header.h"
 #include "draw_hangman.h"
+#include "print_letters.h"
+#include "check_win.h"
 using namespace std;
 
 int main() {
+    string guesses;
     printMessage("HANGMAN");
-    drawHangman(5);
+    drawHangman(9);
+    printAvailableLetters("ALEX");
+    printMessage("Guess the word");
+    printWordAndCheckWin("ALEXES", "ALEX");
     return 0;
 }
